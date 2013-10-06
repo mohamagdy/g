423ed79dc91e95f7983f2a5b08c6447694a43a0f
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-10-06 11:35:48 +0200

    #2, #3 Installing theguardian gem and apply the search API

--- g.gemspec
+++ g.gemspec
@@ -18,6 +18,10 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  # Dependencies
+  spec.add_dependency("theguardian")
+
+  # Dependencies
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
 end
--- lib/g.rb
+++ lib/g.rb
@@ -1,9 +1,37 @@
 require "g/version"
+require "theguardian"
 
+# Gem files
 module G
 	module Command
 		def self.run(command, args)
-			p command, args, "-----"
+			G::Commands::Headline.list(args)
+		end
+	end
+end
+
+module G
+	module Commands
+		module Headline
+			def self.list(params = {})
+				search_params = { 
+					q: params[:q],
+					fields: ["headline"], 
+					date: { 
+						from: params[:from] || Time.now, 
+						to: params[:to] || Time.now 
+					},
+					page: params[:page],
+					per_page: params[:per_page]
+				}
+				
+				items = Theguardian::ContentApi.search(search_params).items
+				items.each { |item| self.pretify(item) }
+			end
+
+			def self.pretify(item)
+				puts "#{Time.parse(item.webPublicationDate).strftime("%Y-%m-%d %H:%M")} - #{item.fields.headline} - #{item.webUrl}"
+			end
 		end
 	end
 end
